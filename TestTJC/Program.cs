9383caa8cbb506b0826ef37d0b71ed427f8bb140
Tony Collins <tjc@eltham-college.org.uk>
2019-07-26 18:30:57 +0100

    Add project files.

---
+++ TestTJC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestTJC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World TC!");
+        }
+    }
+}
